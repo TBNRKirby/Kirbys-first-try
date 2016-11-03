e688be08555b00144be3846dad80a3117793ced3
Matthew <tbnrkirby@gmail.com>
2016-11-03 22:58:12 +0000

    Add geometric_pattern project

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,13 +1,15 @@
-
 function sayThatWasEasy() {
   var thatWasEasy = new Audio("that_was_easy.mp3");
   thatWasEasy.play();
 }
 
+
 $("#easy").on("click", sayThatWasEasy);
 
-$(document).keypress();
+$(document).keypress(delegateKeypress);
 
-function delegateKeypressevent(event) {
-    
+function delegateKeypress(event) {
+  if (event.charCode == 32) {
+    $("#easy").trigger("click");
+  }
 }
